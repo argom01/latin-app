21a1ebd85c51cc6b4fe318aaf724aeb24aa09e46
argom01 <nor.siczek@tutanota.com>
2022-05-02 22:11:48 +0200

    fix set cookie on chrome maybe?

--- src/shared/sendRefreshToken.ts
+++ src/shared/sendRefreshToken.ts
@@ -1,10 +1,12 @@
-import type { Response } from 'express';
+import type { Response } from "express";
 
 const sendRefreshToken = (res: Response, token: string) => {
-	res.cookie('jid', token, {
-		httpOnly: true,
-		path: '/api/v1/auth/refresh_token',
-	});
+    res.cookie("jid", token, {
+        httpOnly: true,
+        sameSite: "none",
+        secure: true,
+        path: "/api/v1/auth/refresh_token",
+    });
 };
 
 export default sendRefreshToken;
